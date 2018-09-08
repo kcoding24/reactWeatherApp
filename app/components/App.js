62833c75a5aa0007144367d7771a4a76f15c7581
kaung myat thu <kcoding24.gmail.com>
2018-09-08 21:20:18 +0800

    initial template

---
+++ app/components/App.js
@@ -0,0 +1,16 @@
+var React = require('react');
+
+
+class App extends React.Component {
+	render() {
+		return (
+			<div className='container'>
+				Hello World!
+			</div>
+		)
+	}
+}
+
+
+
+module.exports = App;
