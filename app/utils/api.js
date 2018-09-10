80be18c8102bfa46be6c31082cd93a94db235590
kaung myat thu <kcoding24.gmail.com>
2018-09-10 21:45:56 +0800

    make getcurrentweather api request

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,8 +1,17 @@
 var axios = require('axios');
 
 function get5DayForecast (city) {
+	var url='http://api.openweathermap.org/data/2.5/forecast?q='+city+'&APPID=505fb23177047802420fd0daaa8c0789'
+	return (
+		axios.get(url)
+			.then(function(weather) {
+				console.log(weather);
+			})
+	)
+}
+
+function getCurrentWeather (city) {
 	var url='http://api.openweathermap.org/data/2.5/weather?q='+city+'&APPID=505fb23177047802420fd0daaa8c0789'
-	var encodedurl = window.encodeURIComponent(url);
 	return (
 		axios.get(url)
 			.then(function(weather) {
@@ -13,4 +22,5 @@ function get5DayForecast (city) {
 
 module.exports = {
 	get5DayForecast: get5DayForecast,
+	getCurrentWeather: getCurrentWeather
 }
