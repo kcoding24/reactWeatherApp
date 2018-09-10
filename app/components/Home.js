d971dbfcaf5342e2266b6a6f66a34476404f7308
kaung myat thu <kcoding24.gmail.com>
2018-09-10 21:14:34 +0800

    Get weather button and input field use api request

--- app/components/Home.js
+++ app/components/Home.js
@@ -4,21 +4,41 @@ var api = require('../utils/api');
 
 class Home extends React.Component {
 
+	constructor(props) {
+		super(props);
+		this.state= {
+			city: '',
+		};
+
+		this.handleChange=this.handleChange.bind(this);
+	}
+
+	handleChange(event) {
+		var value = event.target.value;
+
+		this.setState(function() {
+			return {
+				city: value
+			}
+		});
+	}
+
 
 	render() {
-		api.get5DayForecast('london');
+	
 		return (
 				<div className="home">
 					<h1>Enter a City and State</h1>
 					<input
 						id="city"
 						placeholder="Yangon, Myanmar"
-						type='text'
+						type="text"
+						value={this.state.city}
+						autoComplete="off"
+						onChange={this.handleChange}
 					/>
-					<Link className="button " to="/fiveday">
-
+					<Link className="button " to="/forecast" onClick={api.get5DayForecast.bind(null,this.state.city)}>
 						Get Weather
-					  
 					</Link>
 
 
